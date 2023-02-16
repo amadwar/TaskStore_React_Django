32335af2435d79f89e4ff1be56c5ee285fad494b
amadwar <118168373+amadwar@users.noreply.github.com>
2023-02-16 13:55:10 +0100

    secound commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,13 @@ import Users from './Components/User';
 function App() {
   return (
     <div className="App">
+     
+     <div>
+     <Users/>
+     <br />
+     </div>
+     
+
      <div>
      <TaskTable/>
      <br />
